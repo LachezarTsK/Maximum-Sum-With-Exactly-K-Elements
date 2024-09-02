8d91f209e909348be66f47638f5393eff103ad08
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-09-02 08:55:48 +0200

    Update Solution.cs

--- Solution.cs
+++ Solution.cs
@@ -1,16 +1,18 @@
 
-import kotlin.math.max
+using System;
 
-class Solution {
-
-    fun maximizeSum(input: IntArray, numberOfOperations: Int): Int {
-        var maxValue = 0
-        for (value in input) {
-            maxValue = max(maxValue, value)
+public class Solution
+{
+    public int MaximizeSum(int[] input, int numberOfOperations)
+    {
+        int maxValue = 0;
+        foreach (int value in input)
+        {
+            maxValue = Math.Max(maxValue, value);
         }
         // (numberOfOperations - 1) * numberOfOperations / 2
         // sum of all members of arithmetic progression, where
         // firstMember = 1, step = 1, totalMembers = numberOfOperations - 1
-        return maxValue * numberOfOperations + (numberOfOperations - 1) * numberOfOperations / 2
+        return maxValue * numberOfOperations + (numberOfOperations - 1) * numberOfOperations / 2;
     }
 }
