af80dd299007258df878d976421338f0ecf1dbcb
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-09-02 09:00:25 +0200

    Update Solution.ts

--- Solution.ts
+++ Solution.ts
@@ -1,18 +1,11 @@
 
-#include <algorithm>
-using namespace std;
-
-class Solution {
-
-public:
-    int maximizeSum(vector<int>& input, int numberOfOperations) const {
-        int maxValue = 0;
-        for (const auto& value : input) {
-            maxValue = max(maxValue, value);
-        }
-        // (numberOfOperations - 1) * numberOfOperations / 2
-        // sum of all members of arithmetic progression, where
-        // firstMember = 1, step = 1, totalMembers = numberOfOperations - 1
-        return maxValue * numberOfOperations + (numberOfOperations - 1) * numberOfOperations / 2;
+function maximizeSum(input: number[], numberOfOperations: number): number {
+    let maxValue = 0;
+    for (let value of input) {
+        maxValue = Math.max(maxValue, value);
     }
+    // (numberOfOperations - 1) * numberOfOperations / 2
+    // sum of all members of arithmetic progression, where
+    // firstMember = 1, step = 1, totalMembers = numberOfOperations - 1
+    return maxValue * numberOfOperations + Math.floor((numberOfOperations - 1) * numberOfOperations / 2);
 };
